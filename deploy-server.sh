c2f15ce90b491296d60188968c309832d34cf11a
aduquehd <aduquehd@gmail.com>
2025-07-30 18:48:57 -0500

    Deleted alembic commands since there is not a DB in the project

--- deploy-server.sh
+++ deploy-server.sh
@@ -3,9 +3,6 @@
 git pull
 sudo docker compose -f docker-compose.prod.yml build
 
-# Run DB migrations inside the backend container
-sudo docker compose -f docker-compose.prod.yml run --rm backend uv run alembic upgrade head
-
 # Restart services
 sudo supervisorctl stop json_viewer
 sudo docker compose -f docker-compose.prod.yml down
