79180319d5f348c60e16ffb0181c1679390cbcc0
aduquehd <aduquehd@gmail.com>
2025-07-31 19:30:08 -0500

    Added 404 template and robots.txt

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI, Request
-from fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse, FileResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from fastapi.exceptions import HTTPException
+from starlette.exceptions import HTTPException as StarletteHTTPException
 
 from config import settings
 
@@ -25,6 +27,20 @@ async def read_root(request: Request):
             "ga_tracking_id": settings.ga_tracking_id,
             "app_env": settings.app_env,
         },
+    )
+
+
+@app.get("/robots.txt")
+async def robots():
+    return FileResponse("static/robots.txt", media_type="text/plain")
+
+
+@app.exception_handler(404)
+async def not_found_handler(request: Request, exc: HTTPException):
+    return templates.TemplateResponse(
+        "404.html",
+        {"request": request},
+        status_code=404
     )
 
 
